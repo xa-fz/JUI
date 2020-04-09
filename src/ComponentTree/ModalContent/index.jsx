a8148dda2303b59c1765c7357b7961658df2535a
xa-fz <553866469@qq.com>
2020-04-10 00:55:16 +0800

    【fanzhe】modal弹窗开发：样式属性、点击事件及是否展示浮层

--- src/ComponentTree/ModalContent/index.jsx
+++ src/ComponentTree/ModalContent/index.jsx
@@ -1,26 +1,42 @@
 import React, { Component } from 'react';
 import RESOURCE from '../../resource';
 import Button from '../../Components/Button';
+import Modal from '../../Components/Modal';
 
 export default class ModalContent extends Component{
     constructor(props){
         super(props);
         this.state = {
-
+            isOpen: false
         }
     }
 
     handleClick = () => {
         console.log('点击按钮');
+        this.setState({
+            isOpen: !this.state.isOpen
+        })
     }
 
     render(){
+        const { isOpen } = this.state;
         RESOURCE.MODAL_INFO_ARR.forEach(item => {
             switch(item.type){
                 case 'modal-normal':
                 item.showComponent = 
                 <div className='modalTypeNormal'>
                     <Button type='Primary' text='Open Modal' handleClick={this.handleClick}/>
+                    <Modal 
+                       visible={isOpen}
+                       bodyStyle={{
+                           width:'520px',
+                           height:'260px'
+                       }}
+                    >
+                        <div>
+                            ttttttttttttttttttt
+                        </div>
+                    </Modal>
                 </div>
                 break;
                 default:
--- src/Components/Modal/index.jsx
+++ src/Components/Modal/index.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './modal.less';
 
 export default class Modal extends Component{
     constructor(props){
@@ -7,9 +8,55 @@ export default class Modal extends Component{
 
         }
     }
+    /** 
+     * @method 用于赋值给对话框的margin使垂直居中
+    */
+    judgeType = attr =>{
+        let attrVal;
+        if(typeof attr === 'number'){
+            attrVal = -attr/2
+        }else if(typeof attr === 'string'){
+            attrVal = -parseInt(attr.split('px').join())/2;
+        }
+        return attrVal
+    }
+
+
     render(){
+        const { visible, bodyStyle } = this.props;
+        let margintop;
+        let marginleft;
+        let modalHeight;
+        let modalWidth;
+        if(bodyStyle && bodyStyle.height){
+            modalHeight = bodyStyle.height;
+            margintop = this.judgeType(bodyStyle.height);
+        }else{
+            modalHeight = '270px';
+            margintop = '135px';
+        }
+
+        if(bodyStyle && bodyStyle.width){
+            modalWidth = bodyStyle.width;
+            marginleft = this.judgeType(bodyStyle.width); 
+        }else{
+            modalWidth = '520px';
+            marginleft = '-260px';
+        }
+
         return (
-            ''
+            <div className='jui-modal' style={{display: visible ? 'block' : 'none'}}>
+                <div className='dialog' 
+                    style={{
+                        width: modalWidth,
+                        height: modalHeight,
+                        marginTop: margintop,
+                        marginLeft: marginleft
+                    }}
+                >
+                    <p>123123</p>
+                </div>
+            </div>
         )
     }
 }
