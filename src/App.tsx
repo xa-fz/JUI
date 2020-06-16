1d1bff7199f1b316d287caeba259f27c4643b43b
xa-fz <553866469@qq.com>
2020-06-17 01:10:18 +0800

    完成左树对ts的修改

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,16 @@
-import React,{ Component, useState, ReactElement, useContext } from 'react';
+import React,{ Component, ReactElement, useContext } from 'react';
 import './App.less';
-import ComponentTrees from './ComponentTree/ComponentTree';
+import ComponentsTree from './ComponentTree/ComponentsTree';
 import { ComponentsContext } from './context'
 
 const App: React.FC = (): ReactElement<any, string | (new (props: any) => Component<any, any, any>)> | null =>{
-  const [showComponent, setComponent]: [any, React.Dispatch<React.SetStateAction<any>>] = useState(null);
   const child_component = useContext(ComponentsContext)[0];
 
-
-  const getComponent = (showComponent: any) => {
-    setComponent(showComponent);
-  }
     return (
       <div className="App">
         <div className='componentList'>
           <div className='header'>JUI</div>
-          <ComponentTrees />
+          <ComponentsTree />
         </div>
         <div className='codeContent'>
           { child_component }
